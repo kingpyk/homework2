99bed093aa5721e7a0e77b2819f86b2a7a0a3850
201344036 <kingpyk@naver.com>
2016-09-23 16:17:10 +0900

    성인확인

--- A01.java
+++ A01.java
@@ -1,24 +1,25 @@
 import java.util.*;
 
 class A01 {
+	int age = 0;
+
 	public static void main(String[] s){
-	new A01().born();
+		new A01().born();
 	}
 
-	void born()
-	{
-	Scanner s = new Scanner(System.in);
-	System.out.print(" 태어난 년도를 입력하시오.:");
-	int year = s.nextInt();
-	this.age = 2016 - year +1;
-	adult();
+	void born(){
+		Scanner s = new Scanner(System.in);
+		System.out.print(" 태어난 년도를 입력하시오.:");
+		int year = s.nextInt();
+		this.age = 2016 - year +1;
+		adult();
 	}
-	void adult()
-	{
-	if(this.age <20)
-	System.out.print("미성년자입니다.");
-	else
-	System.out.print("미성년자가 아닙니다.");
+
+	void adult(){
+		if(this.age <20)
+			System.out.print("미성년자입니다.");
+		else
+			System.out.print("미성년자가 아닙니다.");
 	}
 }
 
